6c562f9447547bdadd338d32899fa78246e55eaf
hyundonghan <guehgk456@gmail.com>
2021-06-30 16:38:16 +0900

    setting compose ui-tooling

--- app/src/main/java/dev/dnights/jetpackcompsesample/MainActivity.kt
+++ app/src/main/java/dev/dnights/jetpackcompsesample/MainActivity.kt
@@ -3,13 +3,31 @@ package dev.dnights.jetpackcompsesample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            SimpleComposable()
         }
     }
+}
+
+@Composable
+fun SimpleComposable() {
+    Column{
+        Text("Hello World")
+        Text("Hello World2")
+        Text("Hello World3")
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ComposablePreview() {
+    SimpleComposable()
 }
